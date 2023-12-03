8d33052089e60199c17d450d75ba6de99379103d
Youssef <youssef.benali294@gmail.com>
2023-12-03 17:14:27 +0100

    Add a delete confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,32 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
 
-const DeleteIssueButton = ({issueId}: {issueId: number}) => {
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>Delete Issue</Button>
-  )
-}
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue?
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+         
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
 
-export default DeleteIssueButton
+export default DeleteIssueButton;
