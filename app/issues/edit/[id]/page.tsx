1b12914ceac8e66736047259e3f36842cb5eefc4
Youssef <youssef.benali294@gmail.com>
2023-12-03 17:43:19 +0100

    Remove duplicate skeletons

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -2,7 +2,7 @@ import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 
 import dynamic from "next/dynamic";
-import IssueFormSkeleton from "../loading";
+import IssueFormSkeleton from "../../[id]/loading";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
